a263ead54b32702e0a91fb9beed7efb729b3c962
juliegip <julie.gip@gmail.com>
2023-09-11 21:10:36 +0200

    adding Sass, styling header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "./Header.scss"
+
+function Header(props) {
+    return (
+        <header>
+           <li>
+            Menu
+           </li>
+           <li>
+            Contacts
+           </li>
+           <li>about</li>
+        </header>
+    );
+}
+
+export default Header;
