e80e0abc81eff6e56507efddb5c692db57bb83c5
juliegip <julie.gip@gmail.com>
2023-09-11 21:24:01 +0200

    example css transform

---
+++ src/components/Bouton/Bouton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Bouton.scss'
+
+function Bouton(props) {
+    return (
+        <div className="container">
+            <div className="btn">
+                Vois comme je grandis !
+            </div>
+        </div>
+    );
+}
+
+export default Bouton;
